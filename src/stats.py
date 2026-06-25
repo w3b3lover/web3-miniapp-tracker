99f666cd025133ceb423330f6ba48b67e06585a5
w3b3lover <qoshahin87@gmail.com>
2026-06-26 01:55:08 +0330

    Add tag statistics helper

--- src/stats.py
+++ src/stats.py
@@ -13,3 +13,13 @@ def count_by_status(tracker: MiniAppTracker):
 
 def count_by_ecosystem(tracker: MiniAppTracker):
     return Counter(app.ecosystem for app in tracker.list_apps())
+
+
+def count_by_tag(tracker: MiniAppTracker):
+    counter = Counter()
+
+    for app in tracker.list_apps():
+        for tag in app.tags:
+            counter[tag] += 1
+
+    return counter
