019c6dea1f908f38b0eb7b5b06766ba6f158d7ad
w3b3lover <qoshahin87@gmail.com>
2026-06-26 01:54:04 +0330

    Add mini app statistics helpers

---
+++ src/stats.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+from src.tracker import MiniAppTracker
+
+
+def count_by_category(tracker: MiniAppTracker):
+    return Counter(app.category for app in tracker.list_apps())
+
+
+def count_by_status(tracker: MiniAppTracker):
+    return Counter(app.status for app in tracker.list_apps())
+
+
+def count_by_ecosystem(tracker: MiniAppTracker):
+    return Counter(app.ecosystem for app in tracker.list_apps())
