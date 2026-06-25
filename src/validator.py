177ed08032326b92a30df17b40eb30f418895552
w3b3lover <qoshahin87@gmail.com>
2026-06-26 01:58:20 +0330

    Validate known mini app statuses

--- src/validator.py
+++ src/validator.py
@@ -1,3 +1,5 @@
+from src.status import is_known_status
+
 REQUIRED_FIELDS = ["name", "category", "status"]
 
 
@@ -5,5 +7,8 @@ def validate_miniapp_data(item: dict) -> bool:
     for field in REQUIRED_FIELDS:
         if not item.get(field):
             return False
+
+    if not is_known_status(item.get("status", "")):
+        return False
 
     return True
